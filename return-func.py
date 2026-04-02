6a93e1ddb8a892fdaf0b97fd675f6fe3adde74cc
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-04-02 18:13:34 +0500

    Refactor is_even function and add count_vowels function to check for vowel presence

--- return-func.py
+++ return-func.py
@@ -22,3 +22,33 @@ def toliq_ism_yasa(ism, familiya):
 
 print(toliq_ism_yasa("Olim", "Qodirov"))
 print(toliq_ism_yasa("Anvar", "Aliyev"))
+#4
+def is_even(number):
+   if number % 2 == 0:
+      return "Juft"
+    else:
+      return "Toq"
+   
+print(is_even(4)) # Juft
+result = is_even(7)
+print(result) # Toq 
+ #5
+#Terner operatori yordamida yuqoridagi funksiyani quyidagicha yozish mumkin:
+def is_even(number):
+    return "Juft" if number % 2 == 0 else "Toq"
+
+print(is_even(4)) # Juft
+print(is_even(7)) # Toq
+
+#6
+vowels = ['a', 'e', 'i', 'o', 'u']
+def count_vowels(text):
+    count = 0
+    for char in text:
+       if char == vowels[0] or char == vowels[1] or char == vowels[2] or char == vowels[3] or char == vowels[4]:
+          count += 1
+
+    return count
+print(count_vowels("javascript")) # 3
+print(count_vowels("bbb")) # 0
+print(count_vowels("programming")) # 3
