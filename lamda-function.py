342506d02db566cc569a957c585c1ee242a6a5c9
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-05-05 17:43:15 +0500

    Add lambda function examples demonstrating anonymous functions and exponentiation

---
+++ lamda-function.py
@@ -0,0 +1,7 @@
+#lamda function - anonymous (maxfiy, ma'lum)function
+# lambda argumnts  : expression(ifoda, bajaradigan vazifasi)
+x =lambda a : a % 5
+print(x(12)) # 2
+product = lambda x, y : x ** y
+print(product(3, 2)) # 9
+print(product(8, 3)) # 512
